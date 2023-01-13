93298993fa67e79db02e5fd27c10b25b9633062b
Edwin <edwinscm@users.noreply.github.com>
2023-01-13 18:41:05 -0500

    fix: Removes host in app.listen

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,6 @@
 import app from "./frameworks-drivers/express/app.js";
 import environment from "./environment.js";
 
-app.listen(
-  Number(environment.express.port),
-  String(environment.express.host),
-  () => {
-    console.log(`App listening on port ${environment.express.port}`);
-  }
-);
+app.listen(Number(environment.express.port), () => {
+  console.log(`App listening on port ${environment.express.port}`);
+});
