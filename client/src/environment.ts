1eaf43e9e3415a97d1a185e90ffcb7ceab14172a
Edwin <edwinscm@users.noreply.github.com>
2023-01-13 18:43:26 -0500

    test: Uses api url string instead of env var

--- client/src/environment.ts
+++ client/src/environment.ts
@@ -1,6 +1,7 @@
 const environment = {
-  reactAppApiUrl: process.env.REACT_APP_API_URL,
-  reactAppGoogleApiKey: process.env.REACT_APP_GOOGLE_API_KEY, // Geocoding API, Maps JavaScript API, Places API
+  // apiUrl: process.env.API_URL,
+  apiUrl: "https://api-rental-calculator.up.railway.app",
+  googleApiKey: process.env.GOOGLE_API_KEY, // Geocoding API, Maps JavaScript API, Places API
 };
 
 export default environment;
