291f225c33a349ec3eaa3bffecb70104e51d9ab0
Edwin <edwinscm@users.noreply.github.com>
2023-01-17 00:17:29 -0500

    fix: Uses var env for api url

--- client/src/environment.ts
+++ client/src/environment.ts
@@ -1,6 +1,5 @@
 const environment = {
-  // apiUrl: process.env.API_URL,
-  apiUrl: "https://api-rental-calculator.up.railway.app",
+  apiUrl: process.env.API_URL,
   googleApiKey: process.env.GOOGLE_API_KEY, // Geocoding API, Maps JavaScript API, Places API
 };
 
